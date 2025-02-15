34a88b7f516b2e52501c877fd69fd46b38e2194c
Federico <federicobrandini@gmail.com>
2025-02-15 10:36:27 +0100

    fix: mean waiting time O(1) time complexity

--- src/stats.py
+++ src/stats.py
@@ -7,6 +7,8 @@ class Stats:
     '''
     def __init__(self):
         self._start_end = {}
+        self._sum_waiting_time = 0
+        self._len_executed_orders = 0
 
     def order_arrival(self, order_id: int):
         '''
@@ -19,10 +21,11 @@ class Stats:
         Stores order executed time
         '''
         self._start_end[order_id][1] = time.time()
+        self._sum_waiting_time += self._start_end[order_id][1] - self._start_end[order_id][0]
+        self._len_executed_orders += 1
 
     def mean_waiting_time(self) -> float:
         '''
         Returns mean waiting time so far
         '''
-        executed = dict(filter(lambda item : all(item[1]), self._start_end.items()))
-        return sum(map(lambda item : item[1][1] - item[1][0], executed.items())) / len(executed)
+        return self._sum_waiting_time / self._len_executed_orders
